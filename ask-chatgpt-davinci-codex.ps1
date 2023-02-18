436afd1a59e24596c9cbd66584ddcf95e3ac139d
Tim Sonner <66705347+timsonner@users.noreply.github.com>
2023-02-18 02:58:57 -0700

    initial commit

---
+++ ask-chatgpt-davinci-codex.ps1
@@ -0,0 +1,26 @@
+﻿$apiKey = 'sk-XXXXXXXX....'
+
+$model = 'davinci-codex'
+$temperature = 0.7
+$maxTokens = 60
+
+$prompt = Read-Host "Enter a prompt"
+
+$body = @{
+    prompt = $prompt
+    max_tokens = $maxTokens
+    temperature = $temperature
+} | ConvertTo-Json
+
+$response = Invoke-WebRequest `
+    -Method POST `
+    -Uri "https://api.openai.com/v1/engines/$model/completions" `
+    -Headers @{
+        'Content-Type' = 'application/json'
+        'Authorization' = "Bearer $apiKey"
+    } `
+    -Body $body `
+    -UseBasicParsing
+
+$json = $response.Content | ConvertFrom-Json
+$json.choices.text | ForEach-Object { $_ -replace '\r\n','`r`n' } | Out-String
---
+++ ask-chatgpt-text-davinci-003.ps1
@@ -0,0 +1,22 @@
+﻿$apiKey = "sk-XXXXXXXX..."
+$prompt = Read-Host -Prompt ("Enter a prompt")
+$RequestBody = @{
+    prompt = $prompt
+    model = "text-davinci-003"
+    temperature= 1.0
+    max_tokens= 4000
+}
+
+$Header =@{ Authorization = "Bearer $($apiKey) " }
+$RequestBody=$RequestBody | ConvertTo-Json
+
+$RestMethodParameter=@{
+    Method='Post'
+    Uri ='https://api.openai.com/v1/completions'
+    body=$RequestBody
+    Headers=$Header
+    ContentType='application/json'
+}
+
+
+(Invoke-RestMethod @RestMethodParameter).choices[0].text
