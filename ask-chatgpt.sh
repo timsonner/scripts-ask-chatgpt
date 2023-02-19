2f129f774f48809505482f49e6d60931caf30114
Tim Sonner <66705347+timsonner@users.noreply.github.com>
2023-02-19 01:21:54 -0700

    add user input

--- ask-chatgpt.sh
+++ ask-chatgpt.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+read -p "Enter the prompt: " prompt
+
 #export API_KEY="sk-XXXXXXXXXXXXXXXXXXXx..."
 
 curl https://api.openai.com/v1/completions \
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer $API_KEY" \
-  -d '{
-  "model": "text-davinci-003",
-  "prompt": "Say this is a test",
-  "max_tokens": 7,
-  "temperature": 0
+  -d "{
+  \"model\": \"text-davinci-003\",
+  \"prompt\": \"$prompt\",
+  \"max_tokens\": 4000,
+  \"temperature\": 1
 
-}' | jq -r '.choices | .[] | .text'
+}" | jq -r '.choices | .[] | .text'
+
